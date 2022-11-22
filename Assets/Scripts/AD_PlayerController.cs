57a9893e424f6e084dfc0ebffb21c1719be32bb7
lejan22 <91455326+lejan22@users.noreply.github.com>
2022-11-22 22:04:03 +0100

    snekgaming

--- Assets/Scripts/AD_PlayerController.cs
+++ Assets/Scripts/AD_PlayerController.cs
@@ -38,6 +38,34 @@ public class AD_PlayerController : MonoBehaviour
     //MathF makes all the numbers whole, without decimals
     private void FixedUpdate()
     {
+        for(int i = _segments.Count-1; i > 0; i--)
+        {
+            _segments [i].position = _segments[i - 1].position;
+        }
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+    }
+
+    private void extend()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+    private void ResetGame()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Tube")
+        {
+            extend();
+        } else if(other.tag =="Ouch"){
+            ResetGame();
+        }
+
     }
 }
