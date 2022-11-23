078f3c8c92854c5ce5dc2e25c526c6783095571a
lejan22 <91455326+lejan22@users.noreply.github.com>
2022-11-23 20:39:27 +0100

    Base game done

--- Assets/Scripts/AD_PlayerController.cs
+++ Assets/Scripts/AD_PlayerController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class AD_PlayerController : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
-    private List<Transform> _segments;
+    private List<Transform> _segments;//A list that takes care of how long the snake will get once you eat fruit
+    private Vector2 input; //To prevent the snake from going back
+    private float nextUpdate;
 
     public Transform segmentPrefab;
+    public float speed = 10f;
+    public float speedMultiplier = 1f;
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -18,31 +23,47 @@ public class AD_PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (_direction.x != 0f)
         {
-            _direction = Vector2.up;
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                input = Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                input = Vector2.down;
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.S))
+        // Only allow turning left or right while moving in the y-axis
+        else if (_direction.y != 0f)
         {
-            _direction = Vector2.down;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
-        }else if (Input.GetKeyDown(KeyCode.A))
-        {
-            _direction = Vector2.left;
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                input = Vector2.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                input = Vector2.left;
+            }
         }
     }
 
     //MathF makes all the numbers whole, without decimals
     private void FixedUpdate()
     {
-        for(int i = _segments.Count-1; i > 0; i--)
+        if (input != Vector2.zero)
+        {
+            _direction = input;
+        }
+
+        for (int i = _segments.Count-1; i > 0; i--)
         {
             _segments [i].position = _segments[i - 1].position;
         }
-        this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+        this.transform.position = new Vector3(Mathf.Round(this.transform.position.x)
+            + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+
+
     }
 
     private void extend()
@@ -55,7 +76,14 @@ public class AD_PlayerController : MonoBehaviour
     }
     private void ResetGame()
     {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
 
+        this.transform.position = Vector3.zero;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
