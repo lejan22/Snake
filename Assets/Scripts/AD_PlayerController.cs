ea1763ac1a001f8d15ad717f87adbabe744d8776
lejan22 <alejandrodl2206@gmail.com>
2022-11-29 13:16:52 +0100

    Animations

--- Assets/Scripts/AD_PlayerController.cs
+++ Assets/Scripts/AD_PlayerController.cs
@@ -12,14 +12,27 @@ public class AD_PlayerController : MonoBehaviour
     public Transform segmentPrefab;
     public float speed = 10f;
     public float speedMultiplier = 1f;
-     
+
+    public int life = 3;
+
+    private Animator _animator;
+    private bool isMoving;
+    public GameObject waterParticle;
+    private GameObject turningPoint;
+
     // Start is called before the first frame update
     void Start()
     {
+        life = 3;
         _segments = new List<Transform>();
         _segments.Add(this.transform);
+        turningPoint = transform.Find("AD_turningPoint").gameObject;
     }
 
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -27,11 +40,27 @@ public class AD_PlayerController : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
+                if (waterParticle != null && turningPoint != null)
+                {
+                    Instantiate(waterParticle, turningPoint.transform.position,
+                     turningPoint.transform.rotation);
+                }
+                
+                isMoving = true;
                 input = Vector2.up;
+                _animator.Play("AD_Boa_up");
             }
             else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
+                if (waterParticle != null && turningPoint != null)
+                {
+                    Instantiate(waterParticle, turningPoint.transform.position,
+                     turningPoint.transform.rotation);
+                }
+                
+                isMoving = true;
                 input = Vector2.down;
+                _animator.Play("AD_Boa_down");
             }
         }
         // Only allow turning left or right while moving in the y-axis
@@ -39,11 +68,27 @@ public class AD_PlayerController : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
+                if (waterParticle != null && turningPoint != null)
+                {
+                    Instantiate(waterParticle, turningPoint.transform.position,
+                     turningPoint.transform.rotation);
+                }
+                
+                isMoving = true;
                 input = Vector2.right;
+                _animator.Play("AD_Boa_right");
             }
             else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
+                if (waterParticle != null && turningPoint != null)
+                {
+                    Instantiate(waterParticle, turningPoint.transform.position,
+                     turningPoint.transform.rotation);
+                }
+                
+                isMoving = true;
                 input = Vector2.left;
+                _animator.Play("AD_Boa_left");
             }
         }
     }
@@ -56,9 +101,9 @@ public class AD_PlayerController : MonoBehaviour
             _direction = input;
         }
 
-        for (int i = _segments.Count-1; i > 0; i--)
+        for (int i = _segments.Count - 1; i > 0; i--)
         {
-            _segments [i].position = _segments[i - 1].position;
+            _segments[i].position = _segments[i - 1].position;
         }
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x)
             + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
@@ -91,9 +136,22 @@ public class AD_PlayerController : MonoBehaviour
         if (other.tag == "Tube")
         {
             extend();
-        } else if(other.tag =="Ouch"){
-            ResetGame();
         }
+        else if (other.tag == "Ouch")
+        {
+            life--;
+
+           if (life == 0)
+            {
+                ResetGame();
+                life = 3;
+            }
+           
+        }
+
+    }
+    private void takeDamage(int d)
+    {
 
     }
 }
